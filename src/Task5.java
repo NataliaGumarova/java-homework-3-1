f35a3aa0ba197ac08b7ec6154a71524c954716b1
NataliaGumarova <n.gu.47943@gmail.com>
2025-04-24 18:30:59 +0400

    Задание 5

---
+++ src/Task5.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Task5 {
+    public static void main(String[] args) {
+        System.out.println("Задание № 5");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int N = scan.nextInt();
+        System.out.println("Введите порядок бита, который нужно обнулить");
+        int i = scan.nextInt();
+        int itog = N & ~ (1 << i);
+        String binary = Integer.toBinaryString(itog);
+        System.out.println(binary);
+    }
+}
