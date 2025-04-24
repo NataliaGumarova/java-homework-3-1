912a3e650a224a5bc3c2125ee949f2abe7e03cd5
NataliaGumarova <n.gu.47943@gmail.com>
2025-04-24 15:13:06 +0400

    Задание 2

--- src/Task2.java
+++ src/Task2.java
@@ -6,7 +6,15 @@ public class Task2 {
         System.out.println("Введите значение степени числа 2");
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
-        int itog = 2 << (n - 1);
-        System.out.println(itog);
+        if (n > 0) {
+            int itog = 2 << (n - 1);
+            System.out.println("2 в степени " + n + " = " + itog);
+            } else if (n == 0) {
+            System.out.println("2 в степени " + n + " = " + 1);
+            } else if (n < 0) {
+            int itog = 2 << (-n - 1);
+            System.out.println("2 в степени " + n + " = 1/" + itog);
+        }
+
     }
 }
