a7d525c42d078db8c6d2c9c4a046fefbdaa9b77f
NataliaGumarova <n.gu.47943@gmail.com>
2025-04-27 13:28:52 +0400

    Задание 7

---
+++ src/Task7.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Task7 {
+    public static void main(String[] args) {
+        System.out.println("Задание № 7");
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите число");
+        int N = scan.nextInt();
+        System.out.println("Введите номер бита");
+        int i = scan.nextInt();
+        if ((N & (1<<(i-1))) == (1 << (i - 1))) {
+            String binary = Integer.toBinaryString(N);
+            System.out.println(N + " = " + binary);
+            System.out.println(i + " бит числа " + N + " = 1");
+        } else {
+            String binary = Integer.toBinaryString(N);
+            System.out.println(N + " = " + binary);
+            System.out.println(i + " бит числа " + N + " = 0");
+        }
+    }
+}
