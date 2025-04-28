6febe68ab6458b027b1a3d0eb94477e66989617f
NataliaGumarova <n.gu.47943@gmail.com>
2025-04-28 22:56:21 +0400

    Задание 1

--- src/Task1.java
+++ src/Task1.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
         System.out.println("Задание № 1");
         Scanner scan = new Scanner(System.in);
         System.out.println("Ведите число");
       int number = scan.nextInt();
-      int itog = number | (1 << 0);
+      int itog = number & ~1;
       String binary = Integer.toBinaryString(itog);
       System.out.println(binary);
-
-
     }
 }
