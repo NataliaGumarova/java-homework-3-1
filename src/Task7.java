16ad52714b8aaa7292d54962f92c6f40cadee0a0
NataliaGumarova <n.gu.47943@gmail.com>
2025-04-28 23:34:08 +0400

    Задание 7

--- src/Task7.java
+++ src/Task7.java
@@ -7,9 +7,9 @@ public class Task7 {
         int N = scan.nextInt();
         System.out.println("Введите номер бита");
         int i = scan.nextInt();
-        if ((N & (1<<(i-1))) == (1 << (i - 1))) {
+        if ((N & (1 << i)) == (1 << i)) {
             String binary = Integer.toBinaryString(N);
-            System.out.println(N + " = " + binary);
+            System.out.println(N + " в двоичном виде = " + binary);
             System.out.println(i + " бит числа " + N + " = 1");
         } else {
             String binary = Integer.toBinaryString(N);
